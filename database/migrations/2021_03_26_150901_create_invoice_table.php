ad34d2c900dee00533ccda8289be2f02acf8c7f2
esamban <46743903+esamban@users.noreply.github.com>
2021-03-27 12:58:08 +0300

    Add files via upload

---
+++ database/migrations/2021_03_26_150901_create_invoice_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id('invoice_number');
+            $table->integer('items_number');
+            $table->double('VAT');
+            $table->double('price_befor_VAT');
+            $table->double('discount');
+            $table->double('total_price');
+            $table->string('sales_field');
+            $table->datetime('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice');
+    }
+}
---
+++ database/migrations/2021_03_26_161656_create_income_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('income', function (Blueprint $table) {
+            $table->integer('year');
+            $table->double('Total_Income');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('income');
+    }
+}
